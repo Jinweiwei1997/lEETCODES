a7161b1aff5de0ad0ae3b3d545d744331d7e79e4
Jinweiwei1997 <1091357372@qq.com>
2021-09-27 16:52:20 +0800

    7

---
+++ src/ATOI.java
@@ -0,0 +1,76 @@
+public class ATOI {
+    public static void main(String[] args) {
+        Solution solution=new Solution();
+        int a=solution.myAtoi("4847483646");
+        System.out.println(a);
+    }
+}
+class Solution {
+    public int myAtoi(String s) {
+        int x=0;int flat=1;
+        StringBuffer stringBuffer=new StringBuffer();
+        int i=0;
+        for(;i<s.length();i++)
+            if(s.charAt(i)!=' ')
+                break;
+        if(i==s.length())
+            return 0;
+        if(s.charAt(i)=='+')
+            flat=1;
+        else if (s.charAt(i)=='-')
+            flat=0;
+        else i--;
+        //进行判断是不是0
+        i++;
+        for(;i<s.length();i++)
+            if(s.charAt(i)!='0')
+                break;
+        if(i==s.length()){
+            return 0;
+        }else {
+            for(;i<s.length();i++){
+                if(s.charAt(i)=='0')
+                    stringBuffer.append('0');
+                else if(s.charAt(i)=='1')
+                    stringBuffer.append('1');
+                else if(s.charAt(i)=='2')
+                    stringBuffer.append('2');
+                else if(s.charAt(i)=='3')
+                    stringBuffer.append('3');
+                else if(s.charAt(i)=='4')
+                    stringBuffer.append('4');
+                else if(s.charAt(i)=='5')
+                    stringBuffer.append('5');
+                else if(s.charAt(i)=='6')
+                    stringBuffer.append('6');
+                else if(s.charAt(i)=='7')
+                    stringBuffer.append('7');
+                else if(s.charAt(i)=='8')
+                    stringBuffer.append('8');
+                else  if(s.charAt(i)=='9')
+                    stringBuffer.append('9');
+                else
+                    break;
+            }
+            if (stringBuffer.toString().isEmpty())
+                return 0;
+            String st=stringBuffer.toString();
+            if(st.equals("2147483648") && flat==0) return -2147483648;
+            if(flat==1 && (st.length()>10 )){
+                return 2147483647;
+            }else if(flat==0 && (st.length()>10)) return -2147483648;
+            else{
+                try {
+                    x = Integer.parseInt(st);
+                    if (flat == 0) {
+                        x = -x;
+                    }
+                }catch(Exception e){
+                    if(flat==0) return -2147483648;
+                    else return 2147483647;
+                }
+            }
+    }
+        return x;
+    }
+}
--- src/ReverseNum.java
+++ src/ReverseNum.java
@@ -1,10 +1,60 @@
 public class ReverseNum {
     public static void main(String[] args) {
-
+        Solution7 solution=new Solution7();
+        int x=solution.reverse(-2147483648);
+        System.out.println(x);
     }
 }
-class Solution {
+class Node{
+    public
+        Node next;
+        int value;
+        public Node(){};
+        }
+class Solution7 {
     public int reverse(int x) {
-        return 0;
+        int Total=0;int flat=1;
+        if(x==0||x==-2147483648)
+            return 0;
+        Node node=new Node();
+        Node point=node;
+        if(x<0) {
+            node.value = 11;
+            x=-x;
+        }
+        int Number=0; //表示几位数
+        while(x!=0){
+            int temp=x%10;
+            Node nodeT=new Node();
+            nodeT.value=temp;
+            point.next=nodeT;
+            point=point.next;
+            x/=10;
+            Number++;
+        }
+        int MultiTen=1;
+        while(Number!=1){
+            MultiTen*=10;
+            Number--;
+        }
+        point=node;
+        if(point.value==11){
+            flat=0;//表示负数
+            point=point.next;
+        }else{
+            point=point.next;
+        }
+        int MM=MultiTen;
+        while(point!=null){
+            if(MultiTen==1000000000 && point.value>2 ||(MM==1000000000 && MultiTen==10 &&point.value>4) )
+                return 0;
+            Total+=MultiTen*point.value;
+            MultiTen/=10;
+            point=point.next;
+        }
+        if(flat==0){
+            Total=-Total;
+        }
+        return Total;
     }
 }
